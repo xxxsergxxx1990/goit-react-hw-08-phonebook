01a3e5cac00f7a9c14b25fc1bb02bac56c21ec39
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2023-12-16 18:25:32 +0200

    ......

--- src/services/fetchAuth.js
+++ src/services/fetchAuth.js
@@ -7,4 +7,80 @@ import { options } from 'components/Form/Form';
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+
+const clearAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = '';
+};
+
+const postUser = async (newUser, thunkAPI) => {
+    try {
+        const response = await axios.post('/users/signup', newUser);
+        setAuthHeader(response.data.token);
+        return response.data;
+    } catch (e) {
+        Notify.failure(`We're sorry, something went wrong`, options);
+        return thunkAPI.rejectWithValue(e.message);
+    }
+};
+
+const postLogIn = async (logInUser, thunkAPI) => {
+    try {
+        const response = await axios.post('/users/login', logInUser);
+        setAuthHeader(response.data.token);
+        return response.data;
+    } catch (e) {
+        Notify.failure(`You entered an incorrect login or password`, options);
+        return thunkAPI.rejectWithValue(e.message);
+    }
+};
+
+const postLogOut = async (_, thunkAPI) => {
+    try {
+        await axios.post('/users/logout');
+        clearAuthHeader();
+    } catch (e) {
+        return thunkAPI.rejectWithValue(e.message);
+    }
+};
+
+const getCurrentUser = async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (persistedToken === null) {
+        return thunkAPI.rejectWithValue('Unable to fetch user');
+    };
+
+    try {
+            setAuthHeader(persistedToken);
+        const response = await axios.get('/users/current');
+        return response.data;
+    } catch (e) {
+        return thunkAPI.rejectWithValue(e.message);
+    }
+};
+
+export const postUserThunk = createAsyncThunk(
+    'auth/postUser',
+    postUser
+);
+
+export const postLogInThunk = createAsyncThunk(
+    'auth/postLogIn',
+    postLogIn
+);
+
+export const postLogOutThunk = createAsyncThunk(
+    'auth/postLogOut',
+    postLogOut
+);
+
+export const getCurrentUserThunk = createAsyncThunk(
+    'auth/getCurrentUser',
+    getCurrentUser
+);
 
--- src/services/fetchContacts.js
+++ src/services/fetchContacts.js
@@ -5,3 +5,49 @@ import { options } from 'components/Form/Form';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
+const getContacts = async (_, thunkAPI) => {
+    try {
+        const response = await axios.get('/contacts');
+return response.data;
+    }
+    catch (e) {
+        return thunkAPI.rejectWithValue(e.message);
+    }
+};
+
+const postContact = async (newContact, thunkAPI) => {
+    try {
+        const response = await axios.post('/contacts', newContact);
+        Notify.success(`Contact added successfully`, options);
+        return response.data;
+    }
+    catch (e) {
+        return thunkAPI.rejectWithValue(e.message);
+    }
+};
+
+const delContact = async (contactId, thunkAPI) => {
+    try {
+        const response = await axios.delete(`/contacts/${contactId}`);
+                    Notify.warning(`Contact delete successfully`, options);
+return response.data;
+    }
+    catch (e) {
+        return thunkAPI.rejectWithValue(e.message);
+    }
+};
+
+export const getContactsThunk = createAsyncThunk(
+    'phoneBook/getContacts',
+    getContacts
+);
+
+export const postContactThunk = createAsyncThunk(
+    'phoneBook/postContact',
+    postContact
+);
+    
+export const delContactThunk = createAsyncThunk(
+    'phoneBook/delContact',
+    delContact
+);
